c86eb57194d2e7708d3ab629c8917d9ee849bb80
inux <inux.steve@gmail.com>
2018-10-11 20:12:08 +0200

    started pong impl

--- src/02_exercise/FragmentShader.glsl
+++ src/02_exercise/FragmentShader.glsl
@@ -1,5 +1,6 @@
 precision mediump float;
 
+varying vec4 vColor;
 varying vec2 vTextureCoord;
 
 uniform sampler2D uSampler;
--- src/02_exercise/VertexShader.glsl
+++ src/02_exercise/VertexShader.glsl
@@ -1,10 +1,12 @@
 attribute vec2 aVertexTextureCoord;
 attribute vec2 aVertexPosition;
 
+varying vec2 vColor;
 varying vec2 vTextureCoord;
 
 void main(void)
 {
-    gl_Position = vec4(aVertexPosition, 1.0, 1.0);
+    gl_Position = vec4(aVertexPosition, 0.0, 1.0);
+    vColor = aVertexColor;
     vTextureCoord = aVertexTextureCoord;
 }
