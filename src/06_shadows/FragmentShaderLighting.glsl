4c58cc573dcecea13746dee2f3e29c4b5cff8eff
inux <inux.steve@gmail.com>
2018-12-04 11:15:38 +0100

    add shiny

--- src/06_shadows/FragmentShaderLighting.glsl
+++ src/06_shadows/FragmentShaderLighting.glsl
@@ -5,7 +5,6 @@ uniform bool uEnableLighting;
 
 uniform vec3 uLightPosition;
 uniform vec3 uLightColor;
-//varying vec3 vLightPositionEye3;
 
 varying vec3 vNormalEye;
 varying vec3 vVertexPositionEye3;
@@ -14,10 +13,8 @@ varying vec3 vColor;
 varying vec2 vTextureCoord;
 uniform sampler2D uSampler;
 
-
 const float ambientFactor = 0.2;
-const float shininess = 10.0;
-const vec3 specularMaterialColor = vec3(0.4, 0.4, 0.4);
+const vec3 specularMaterialColor = vec3(0.5, 0.5, 0.5);
 
 void main() {
     vec3 baseColor = vColor;
@@ -27,7 +24,7 @@ void main() {
 
     gl_FragColor = vec4(baseColor, 1.0);
 
-  if (uEnableLighting) {
+    if (uEnableLighting) {
         // calculate light direction as seen from the vertex position
         vec3 lightDirectionEye = normalize(uLightPosition - vVertexPositionEye3);
         vec3 normal = normalize(vNormalEye);
@@ -36,23 +33,25 @@ void main() {
         vec3 ambientColor = ambientFactor * baseColor.rgb;
 
         // diffuse lighting
-        float diffuseFactor = max(dot(normal, lightDirectionEye), 0.0);
-        vec3 diffuseColor = diffuseFactor * uLightColor * baseColor;
+        float diffuseFactor = 0.45;
+        float cosAngle = dot(normal, lightDirectionEye);
+        vec3 diffuseColor = diffuseFactor * (clamp(cosAngle, 0.0, 1.0) * baseColor.rgb);
 
         // specular lighting
         vec3 specularColor = vec3(0, 0, 0);
         if (diffuseFactor > 0.0) {
-           vec3 reflectionDir = reflect(-uLightPosition, normal);
-           vec3 eyeDir = normalize(-vVertexPositionEye3);
-           float cosPhi = max(dot(reflectionDir, eyeDir), 0.0);
-           float specularFactor = pow(cosPhi, shininess);
-           specularColor = specularFactor * specularColor * specularMaterialColor;
+           vec3 reflectionDir = normalize(2.0 + cosAngle) * normal - lightDirectionEye;
+           vec3 eyeDir = normalize(-1.0 * vVertexPositionEye3);
+           float cosPhi = clamp(dot(reflectionDir, eyeDir), 0.0 , 1.0);
+           cosPhi = pow(cosPhi, 10.0);
+           float specularFactor = 0.45;
+           specularColor = specularFactor * specularMaterialColor + uLightColor * cosPhi;
         }
-
         vec3 color = ambientColor + diffuseColor + specularColor;
         gl_FragColor = vec4(color, 1.0);
     }
     else {
         gl_FragColor = vec4(baseColor, 1.0);
     }
+
 }
